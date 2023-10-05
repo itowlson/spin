ea578c84aa2e36bf92f0c3853b5d696c6498f34a
itowlson <ivan.towlson@fermyon.com>
2023-10-05 13:32:36 +1300

    Complete the top-level commands

--- src/bin/completions.rs
+++ src/bin/completions.rs
@@ -1,4 +1,6 @@
+use clap::CommandFactory;
 use shell_completion::{CompletionInput, CompletionSet};
+use spin_cli::SpinApp;
 
 fn main() -> anyhow::Result<()> {
     let input = shell_completion::BashCompletionInput::from_env().unwrap();
@@ -17,10 +19,35 @@ fn complete(input: impl CompletionInput) -> Vec<String> {
     }
 }
 
-fn complete_spin_commands(_input: impl CompletionInput) -> Vec<String> {
-    todo!()
+fn complete_spin_commands(input: impl CompletionInput) -> Vec<String> {
+    let command = SpinApp::command();
+
+    // --help and --version don't show up as options so this doesn't complete them,
+    // but I'm not going to lose much sleep over that.
+
+    // TODO: this doesn't currently offer plugin names as completions.
+
+    let candidates = command.get_subcommands().filter(|c| !c.is_hide_set()).map(|c| c.get_name());
+    input.complete_subcommand(candidates)
 }
 
 fn complete_spin_subcommand(_subcommand: &str, _input: impl CompletionInput) -> Vec<String> {
-    todo!()
+    vec![]
+}
+
+trait ArgInfo {
+    fn long_and_short(&self) -> Vec<String>;
+}
+
+impl<'a> ArgInfo for clap::Arg<'a> {
+    fn long_and_short(&self) -> Vec<String> {
+        let mut result = vec![];
+        if let Some(c) = self.get_short() {
+            result.push(format!("-{c}"));
+        }
+        if let Some(s) = self.get_long() {
+            result.push(format!("--{s}"));
+        }
+        result
+    }
 }
