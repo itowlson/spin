69130a1159b7988ee111dfe0318b81e17ef286d4
itowlson <ivan.towlson@fermyon.com>
2024-10-10 12:31:29 +1300

    Extremely interim commit, basically at the "it compiles, preserve it quick" stage

---
+++ crates/factor-blobstore/src/host.rs
@@ -0,0 +1,289 @@
+use anyhow::{Context, Result};
+use spin_core::{async_trait, wasmtime::component::Resource};
+use spin_resource_table::Table;
+use spin_world::wasi::blobstore;
+use std::{collections::HashSet, sync::Arc};
+// use tracing::{instrument, Level};
+
+const DEFAULT_STORE_TABLE_CAPACITY: u32 = 256;
+
+pub use blobstore::types::Error;
+
+#[async_trait]
+pub trait ContainerManager: Sync + Send {
+    async fn get(&self, name: &str) -> Result<Arc<dyn Container>, Error>;
+    fn is_defined(&self, container_name: &str) -> bool;
+
+    /// A human-readable summary of the given container's configuration
+    ///
+    /// Example: "Azure blob container 'biscuit-tin'"
+    fn summary(&self, store_name: &str) -> Option<String> {
+        let _ = store_name;
+        None
+    }
+}
+
+#[async_trait]
+pub trait Container: Sync + Send {
+    async fn exists(&self) -> anyhow::Result<bool>;
+}
+
+pub struct BlobStoreDispatch {
+    allowed_containers: HashSet<String>,
+    manager: Arc<dyn ContainerManager>,
+    containers: Table<Arc<dyn Container>>,
+}
+
+impl BlobStoreDispatch {
+    pub fn new(allowed_containers: HashSet<String>, manager: Arc<dyn ContainerManager>) -> Self {
+        Self::new_with_capacity(allowed_containers, manager, DEFAULT_STORE_TABLE_CAPACITY)
+    }
+
+    pub fn new_with_capacity(
+        allowed_container: HashSet<String>,
+        manager: Arc<dyn ContainerManager>,
+        capacity: u32,
+    ) -> Self {
+        Self {
+            allowed_containers: allowed_container,
+            manager,
+            containers: Table::new(capacity),
+        }
+    }
+
+    pub fn get_container(&self, container: Resource<blobstore::blobstore::Container>) -> anyhow::Result<&Arc<dyn Container>> {
+        self.containers.get(container.rep()).context("invalid container")
+    }
+
+    pub fn allowed_stores(&self) -> &HashSet<String> {
+        &self.allowed_containers
+    }
+}
+
+#[async_trait]
+impl blobstore::blobstore::Host for BlobStoreDispatch {
+    async fn create_container(&mut self, _name: String) -> Result<Resource<blobstore::container::Container>, String> {
+        Err("This version of Spin does not support creating containers".to_owned())
+    }
+
+    async fn get_container(&mut self, name: String) -> Result<Resource<blobstore::container::Container>, String> {
+        if self.allowed_containers.contains(&name) {
+            let container = self.manager.get(&name).await?;
+            let rep = self.containers.push(container).unwrap();
+            Ok(Resource::new_own(rep))
+        } else {
+            Err("forbidden container".to_owned())
+        }
+    }
+
+    async fn delete_container(&mut self, _name: String) -> Result<(), String> {
+        Err("This version of Spin does not support deleting containers".to_owned())
+    }
+
+    async fn container_exists(&mut self, name: String) -> Result<bool, String> {
+        if self.allowed_containers.contains(&name) {
+            let container = self.manager.get(&name).await?;
+            container.exists().await.map_err(|e| e.to_string())
+        } else {
+            Ok(false)
+        }
+    }
+
+    async fn copy_object(&mut self, _src: blobstore::blobstore::ObjectId, _dest: blobstore::blobstore::ObjectId) -> Result<(), String> {
+        Err("This version of Spin does not support copying objects".to_owned())
+    }
+
+    async fn move_object(&mut self, _src: blobstore::blobstore::ObjectId, _dest: blobstore::blobstore::ObjectId) -> Result<(), String> {
+        Err("This version of Spin does not support moving objects".to_owned())
+    }
+}
+
+#[async_trait]
+impl blobstore::types::Host for BlobStoreDispatch {
+    fn convert_error(&mut self, error: String) -> anyhow::Result<String> {
+        Ok(error)
+    }
+}
+
+#[async_trait]
+impl blobstore::types::HostIncomingValue for BlobStoreDispatch {
+    async fn incoming_value_consume_sync(&mut self, self_: Resource<blobstore::types::IncomingValue>) -> Result<Vec<u8>, String> {
+        todo!()
+    }
+
+    async fn incoming_value_consume_async(&mut self, self_: Resource<blobstore::types::IncomingValue>) -> Result<Resource<blobstore::types::InputStream>, String> {
+        todo!()
+    }
+
+    async fn size(&mut self, self_: Resource<blobstore::types::IncomingValue>) -> anyhow::Result<u64> {
+        todo!()
+    }
+
+    async fn drop(&mut self, rep: Resource<blobstore::types::IncomingValue>) -> anyhow::Result<()> {
+        todo!()
+    }
+}
+
+#[async_trait]
+impl blobstore::types::HostOutgoingValue for BlobStoreDispatch {
+    async fn new_outgoing_value(&mut self) -> anyhow::Result<Resource<blobstore::types::OutgoingValue>> {
+        todo!()
+    }
+
+    async fn outgoing_value_write_body(&mut self, self_: Resource<blobstore::types::OutgoingValue>) -> anyhow::Result<Result<Resource<blobstore::types::OutputStream>, ()>> {
+        todo!()
+    }
+
+    async fn finish(&mut self, self_: Resource<blobstore::types::OutgoingValue>) -> Result<(), String> {
+        todo!()
+    }
+
+    async fn drop(&mut self, rep: Resource<blobstore::types::OutgoingValue>) -> anyhow::Result<()> {
+        todo!()
+    }
+}
+
+// TODO: TBD if these belong on BSD or some other struct (like the one that maps to a Container resource JUST SAYIN)
+#[async_trait]
+impl blobstore::container::Host for BlobStoreDispatch {}
+
+#[async_trait]
+impl blobstore::container::HostContainer for BlobStoreDispatch {
+    async fn name(&mut self, self_: Resource<blobstore::container::Container>) -> Result<String, String> {
+        todo!()
+    }
+
+    async fn info(&mut self, self_: Resource<blobstore::container::Container>) -> Result<blobstore::container::ContainerMetadata, String> {
+        todo!()
+    }
+
+    async fn get_data(&mut self, self_: Resource<blobstore::container::Container>, name: blobstore::container::ObjectName, start: u64, end: u64) -> Result<Resource<blobstore::types::IncomingValue>, String> {
+        todo!()
+    }
+
+    async fn write_data(&mut self, self_: Resource<blobstore::container::Container>, name: blobstore::container::ObjectName, data: Resource<blobstore::types::OutgoingValue>) -> Result<(), String> {
+        todo!()
+    }
+
+    async fn list_objects(&mut self, self_: Resource<blobstore::container::Container>) -> Result<Resource<blobstore::container::StreamObjectNames>, String> {
+        todo!()
+    }
+
+    async fn delete_object(&mut self, self_: Resource<blobstore::container::Container>, name: blobstore::container::ObjectName) -> Result<(), String> {
+        todo!()
+    }
+
+    async fn delete_objects(&mut self, self_: Resource<blobstore::container::Container>, names: Vec<blobstore::container::ObjectName>) -> Result<(), String> {
+        todo!()
+    }
+
+    async fn has_object(&mut self, self_: Resource<blobstore::container::Container>, name: blobstore::container::ObjectName) -> Result<bool, String> {
+        todo!()
+    }
+
+    async fn object_info(&mut self, self_: Resource<blobstore::container::Container>, name: blobstore::container::ObjectName) -> Result<blobstore::types::ObjectMetadata, String> {
+        todo!()
+    }
+
+    async fn clear(&mut self, self_: Resource<blobstore::container::Container>) -> Result<(), String> {
+        todo!()
+    }
+
+    async fn drop(&mut self, rep: Resource<blobstore::container::Container>) -> anyhow::Result<()> {
+        todo!()
+    }
+}
+
+#[async_trait]
+impl blobstore::container::HostStreamObjectNames for BlobStoreDispatch {
+    async fn read_stream_object_names(&mut self, self_: Resource<blobstore::container::StreamObjectNames>, len: u64) -> Result<(Vec<blobstore::container::ObjectName>,bool), String> {
+        todo!()
+    }
+
+    async fn skip_stream_object_names(&mut self, self_: Resource<blobstore::container::StreamObjectNames>, num: u64) -> Result<(u64,bool), String> {
+        todo!()
+    }
+
+    async fn drop(&mut self, rep: Resource<blobstore::container::StreamObjectNames>) -> anyhow::Result<()> {
+        todo!()
+    }
+}
+
+// #[async_trait]
+// impl blobstore::HostStore for BlobStoreDispatch {
+//     // #[instrument(name = "spin_blobstore.open", skip(self), err(level = Level::INFO), fields(otel.kind = "client", kv.backend=self.manager.summary(&name).unwrap_or("unknown".to_string())))]
+//     // async fn open(&mut self, name: String) -> Result<Result<Resource<blobstore::blobstore::Container>, Error>> {
+//     //     Ok(async {
+//     //         if self.allowed_stores.contains(&name) {
+//     //             let store = self
+//     //                 .stores
+//     //                 .push(self.manager.get(&name).await?)
+//     //                 .map_err(|()| Error::StoreTableFull)?;
+//     //             Ok(Resource::new_own(store))
+//     //         } else {
+//     //             Err(Error::AccessDenied)
+//     //         }
+//     //     }
+//     //     .await)
+//     // }
+
+//     // #[instrument(name = "spin_key_value.get", skip(self, store, key), err(level = Level::INFO), fields(otel.kind = "client"))]
+//     // async fn get(
+//     //     &mut self,
+//     //     store: Resource<blobstore::blobstore::Container>,
+//     //     key: String,
+//     // ) -> Result<Result<Option<Vec<u8>>, Error>> {
+//     //     let store = self.get_store(store)?;
+//     //     Ok(store.get(&key).await)
+//     // }
+
+//     // #[instrument(name = "spin_key_value.set", skip(self, store, key, value), err(level = Level::INFO), fields(otel.kind = "client"))]
+//     // async fn set(
+//     //     &mut self,
+//     //     store: Resource<key_value::Store>,
+//     //     key: String,
+//     //     value: Vec<u8>,
+//     // ) -> Result<Result<(), Error>> {
+//     //     let store = self.get_store(store)?;
+//     //     Ok(store.set(&key, &value).await)
+//     // }
+
+//     // #[instrument(name = "spin_key_value.delete", skip(self, store, key), err(level = Level::INFO), fields(otel.kind = "client"))]
+//     // async fn delete(
+//     //     &mut self,
+//     //     store: Resource<key_value::Store>,
+//     //     key: String,
+//     // ) -> Result<Result<(), Error>> {
+//     //     let store = self.get_store(store)?;
+//     //     Ok(store.delete(&key).await)
+//     // }
+
+//     // #[instrument(name = "spin_key_value.exists", skip(self, store, key), err(level = Level::INFO), fields(otel.kind = "client"))]
+//     // async fn exists(
+//     //     &mut self,
+//     //     store: Resource<key_value::Store>,
+//     //     key: String,
+//     // ) -> Result<Result<bool, Error>> {
+//     //     let store = self.get_store(store)?;
+//     //     Ok(store.exists(&key).await)
+//     // }
+
+//     // #[instrument(name = "spin_key_value.get_keys", skip(self, store), err(level = Level::INFO), fields(otel.kind = "client"))]
+//     // async fn get_keys(
+//     //     &mut self,
+//     //     store: Resource<key_value::Store>,
+//     // ) -> Result<Result<Vec<String>, Error>> {
+//     //     let store = self.get_store(store)?;
+//     //     Ok(store.get_keys().await)
+//     // }
+
+//     async fn drop(&mut self, store: Resource<blobstore::blobstore::Container>) -> Result<()> {
+//         self.stores.remove(store.rep());
+//         Ok(())
+//     }
+// }
+
+pub fn log_error(err: impl std::fmt::Debug) -> String {
+    tracing::warn!("blobstore error: {err:?}");
+    format!("{err:?}")
+}
---
+++ crates/factor-blobstore/src/lib.rs
@@ -0,0 +1,158 @@
+mod host;
+pub mod runtime_config;
+mod util;
+
+use std::{
+    collections::{HashMap, HashSet},
+    sync::Arc,
+};
+
+use anyhow::ensure;
+use spin_factors::{
+    ConfigureAppContext, Factor, FactorInstanceBuilder, InitContext, PrepareContext, RuntimeFactors,
+};
+use spin_locked_app::MetadataKey;
+
+/// Metadata key for key-value stores.
+pub const BLOB_STORES_KEY: MetadataKey<Vec<String>> = MetadataKey::new("blob_containers");
+pub use host::{log_error, Error, BlobStoreDispatch, Container, ContainerManager};
+pub use runtime_config::RuntimeConfig;
+pub use util::{CachingStoreManager, DelegatingContainerManager};
+
+/// A factor that provides key-value storage.
+#[derive(Default)]
+pub struct BlobStoreFactor {
+    _priv: (),
+}
+
+impl BlobStoreFactor {
+    /// Create a new KeyValueFactor.
+    pub fn new() -> Self {
+        Self { _priv: () }
+    }
+}
+
+impl Factor for BlobStoreFactor {
+    type RuntimeConfig = RuntimeConfig;
+    type AppState = AppState;
+    type InstanceBuilder = InstanceBuilder;
+
+    fn init<T: Send + 'static>(&mut self, mut ctx: InitContext<T, Self>) -> anyhow::Result<()> {
+        ctx.link_bindings(spin_world::wasi::blobstore::blobstore::add_to_linker)?;
+        Ok(())
+    }
+
+    fn configure_app<T: RuntimeFactors>(
+        &self,
+        mut ctx: ConfigureAppContext<T, Self>,
+    ) -> anyhow::Result<Self::AppState> {
+        let store_managers = ctx.take_runtime_config().unwrap_or_default();
+
+        let delegating_manager = DelegatingContainerManager::new(store_managers);
+        // let caching_manager = CachingStoreManager::new(delegating_manager);
+        let store_manager = Arc::new(delegating_manager);
+
+        // Build component -> allowed stores map
+        let mut component_allowed_stores = HashMap::new();
+        for component in ctx.app().components() {
+            let component_id = component.id().to_string();
+            let containers = component
+                .get_metadata(BLOB_STORES_KEY)?
+                .unwrap_or_default()
+                .into_iter()
+                .collect::<HashSet<_>>();
+            for label in &containers {
+                // TODO: port nicer errors from KeyValueComponent (via error type?)
+                ensure!(
+                    store_manager.is_defined(label),
+                    "unknown key_value_stores label {label:?} for component {component_id:?}"
+                );
+            }
+            component_allowed_stores.insert(component_id, containers);
+            // TODO: warn (?) on unused store?
+        }
+
+        Ok(AppState {
+            container_manager: store_manager,
+            component_allowed_stores,
+        })
+    }
+
+    fn prepare<T: RuntimeFactors>(
+        &self,
+        ctx: PrepareContext<T, Self>,
+    ) -> anyhow::Result<InstanceBuilder> {
+        let app_state = ctx.app_state();
+        let allowed_stores = app_state
+            .component_allowed_stores
+            .get(ctx.app_component().id())
+            .expect("component should be in component_stores")
+            .clone();
+        Ok(InstanceBuilder {
+            store_manager: app_state.container_manager.clone(),
+            allowed_stores,
+        })
+    }
+}
+
+type AppStoreManager = DelegatingContainerManager;
+
+pub struct AppState {
+    /// The store manager for the app.
+    ///
+    /// This is a cache around a delegating store manager. For `get` requests,
+    /// first checks the cache before delegating to the underlying store
+    /// manager.
+    container_manager: Arc<AppStoreManager>,
+    /// The allowed stores for each component.
+    ///
+    /// This is a map from component ID to the set of store labels that the
+    /// component is allowed to use.
+    component_allowed_stores: HashMap<String, HashSet<String>>,
+}
+
+impl AppState {
+    /// Returns the [`StoreManager::summary`] for the given store label.
+    pub fn store_summary(&self, label: &str) -> Option<String> {
+        self.container_manager.summary(label)
+    }
+
+    /// Returns true if the given store label is used by any component.
+    pub fn store_is_used(&self, label: &str) -> bool {
+        self.component_allowed_stores
+            .values()
+            .any(|stores| stores.contains(label))
+    }
+
+    /// Get a store by label.
+    pub async fn get_container(&self, label: &str) -> Option<Arc<dyn Container>> {
+        self.container_manager.get(label).await.ok()
+    }
+}
+
+pub struct InstanceBuilder {
+    /// The store manager for the app.
+    ///
+    /// This is a cache around a delegating store manager. For `get` requests,
+    /// first checks the cache before delegating to the underlying store
+    /// manager.
+    store_manager: Arc<AppStoreManager>,
+    /// The allowed stores for this component instance.
+    allowed_stores: HashSet<String>,
+}
+
+impl FactorInstanceBuilder for InstanceBuilder {
+    type InstanceState = BlobStoreDispatch;
+
+    fn build(self) -> anyhow::Result<Self::InstanceState> {
+        let Self {
+            store_manager,
+            allowed_stores,
+        } = self;
+        Ok(BlobStoreDispatch::new_with_capacity(
+            allowed_stores,
+            store_manager,
+            u32::MAX,
+        ))
+    }
+}
