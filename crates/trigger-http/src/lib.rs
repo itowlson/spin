4b80cb1161070d4662187d25412697d88f99fe9f
itowlson <ivan.towlson@fermyon.com>
2025-11-11 14:39:32 +1300

    HOLY GUACAMOLE

--- crates/trigger-http/src/lib.rs
+++ crates/trigger-http/src/lib.rs
@@ -2,6 +2,7 @@
 
 mod headers;
 mod instrument;
+mod middleware;
 mod outbound_http;
 mod server;
 mod spin;
@@ -110,6 +111,10 @@ impl<F: RuntimeFactors> Trigger<F> for HttpTrigger {
         server.serve().await?;
 
         Ok(())
+    }
+
+    fn complicator() -> impl spin_factors_executor::Complicator {
+        middleware::HttpMiddlewareComplicator
     }
 
     fn supported_host_requirements() -> Vec<&'static str> {
